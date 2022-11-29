183651619dd38dbf048cda2b4da9fe28901ee222
em1ko <emicka.petrova@gmail.com>
2022-11-29 20:41:29 +0100

    ukol

---
+++ KYRBY.py
@@ -0,0 +1,24 @@
+def read(path):
+    try:
+        file = open(path,"r")
+       
+    except FileNotFoundError:
+        print("Soubor nenalezen :c")
+        return[]
+    obsah = file.read()
+    file.close()
+    return obsah
+
+print (read("nacteni.txt"))
+
+def look_for(key,worth,dictionary):
+    try:
+        value=dictionary[key]
+        
+    except KeyError:
+        print("skoda")
+    if worth==value:
+        print("shoda")
+    else:
+        print("neshoda")
+look_for("rohlik","obed",{"rohlik":"obed"})
